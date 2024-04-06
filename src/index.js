edff36e78e4c80a3c273f4da230c87c2a0c9ce48
Nicolas Canales Diaz <nicolasjcanalesdiaz@gmail.com>
2024-04-06 17:39:04 -0300

    error de conexion

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ let refs = {
 };
 let params = new URLSearchParams({
     key: API_KEY,
+    q: '',
     page: page,
     per_page: 40,
     image_type: "photo",
@@ -27,15 +28,16 @@ refs.form.addEventListener("submit", async e => {
         params.set('page',1);
     }
     document.getElementById("gallery").innerHTML = "";
-    params.append('q', q);
+    params.set('q', q);
     await fetchImages(params);
-    console.log(params.get('page'));
+    console.log(params.toString());
     return params;
 });
 
 async function fetchImages(params) { 
     try {
         let response = await axios.get(BASE_URL + params);
+        console.log(response);
         data = response.data.hits;
         if (data.length === 0) {
             Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
